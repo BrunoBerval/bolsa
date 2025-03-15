2cbeb7aac60fa4a463517fed2b2f3da0fa016f9b
Bruno Berval <brunoberval012@gmail.com>
2025-03-15 17:13:12 -0300

    finalização da parte em go

--- internal/market/entity/order.go
+++ internal/market/entity/order.go
@@ -9,4 +9,19 @@ type Order struct {
 	Price         float64
 	OrderType     string
 	Status        string
+	Transactions  []*Transaction
+}
+
+func NewOrder(id string, investor *Investor, asset *Asset, shares int, price float64, orderType string) *Order {
+	return &Order{
+		ID:            id,
+		Investor:      investor,
+		Asset:         asset,
+		Shares:        shares,
+		PendingShares: shares,
+		Price:         price,
+		OrderType:     orderType,
+		Status:        "OPEN",
+		Transactions:  []*Transaction{},
+	}
 }
